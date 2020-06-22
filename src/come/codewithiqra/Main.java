1f61fe75bdff1f164b994ea8836d2d85558c85d1
HP 6th <iqra615>
2020-06-22 07:48:59 -0700

    Calender_getInstance

---
+++ src/come/codewithiqra/Main.java
@@ -0,0 +1,14 @@
+package come.codewithiqra;
+
+import java.util.Calendar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calendar c = Calendar.getInstance();
+        System.out.println("The Current Date is: " + c.getTime());
+
+
+    }
+}
